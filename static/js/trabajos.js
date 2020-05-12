53b65165cc72c2e13b1930ec297457e7fe27d38f
TheSumsito <mjaral@outlook.com>
2020-05-11 22:51:05 -0400

    Footer Creado y vistas ajustadas

--- static/js/trabajos.js
+++ static/js/trabajos.js
@@ -11,6 +11,8 @@ $(document).ready(function (){
         $('.container-trabajos .contenido .caja-decorativa').addClass('remove');
         $('.container-trabajos .contenido .retratos').addClass('remove');
         $('.container-trabajos .contenido .cuadros').addClass('remove');
+
+        $('.container-trabajos').css({'grid-template-rows':'repeat(13, 100px)'});
     }
 
     document.getElementById('oleo').onclick = function (){
@@ -20,6 +22,8 @@ $(document).ready(function (){
         $('.container-trabajos .contenido .caja-decorativa').addClass('remove');
         $('.container-trabajos .contenido .retratos').addClass('remove');
         $('.container-trabajos .contenido .cuadros').addClass('remove');
+
+        $('.container-trabajos').css({'grid-template-rows':'repeat(15, 87px)'});
     }
 
     document.getElementById('cajas').onclick = function (){
@@ -29,6 +33,7 @@ $(document).ready(function (){
         $('.container-trabajos .contenido .oleo').addClass('remove');
         $('.container-trabajos .contenido .retratos').addClass('remove');
         $('.container-trabajos .contenido .cuadros').addClass('remove');
+        $('.container-trabajos').css({'grid-template-rows':'repeat(17, 98px)'});
     }
 
     document.getElementById('retratos').onclick = function (){
@@ -38,6 +43,8 @@ $(document).ready(function (){
         $('.container-trabajos .contenido .oleo').addClass('remove');
         $('.container-trabajos .contenido .caja-decorativa').addClass('remove');
         $('.container-trabajos .contenido .cuadros').addClass('remove');
+
+        $('.container-trabajos').css({'grid-template-rows':'repeat(16, 96px)'});
     }
 
     document.getElementById('cuadros').onclick = function (){
@@ -47,5 +54,7 @@ $(document).ready(function (){
         $('.container-trabajos .contenido .oleo').addClass('remove');
         $('.container-trabajos .contenido .caja-decorativa').addClass('remove');
         $('.container-trabajos .contenido .retratos').addClass('remove');
+
+        $('.container-trabajos').css({'grid-template-rows':'repeat(18, 101px)'});
     }
 });
